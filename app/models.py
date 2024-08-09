9da21b2b0bbb4fd3a1c33eba635e88cca263d61e
Jonathan Lane <admiralorbiter@gmail.com>
2024-08-08 21:32:58 -0500

    updated artist

--- app/models.py
+++ app/models.py
@@ -1,5 +1,17 @@
 from app import db
 
+class Artist(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    artist_name = db.Column(db.String(120), nullable=False)
+    need_to_explore = db.Column(db.Boolean, nullable=False)
+    looked_at = db.Column(db.Boolean, nullable=False)
+    artist_playlist = db.Column(db.Boolean, nullable=False)
+    one_of_each = db.Column(db.String(120), nullable=True)
+    notes = db.Column(db.Text, nullable=True)
+
+    def __repr__(self):
+        return f'<Artist {self.artist_name}>'
+
 class Track(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     track_uri = db.Column(db.String(100), nullable=False, unique=True)
--- app/views.py
+++ app/views.py
@@ -1,8 +1,7 @@
-import csv
-import io
-from flask import render_template
-from app import app
-from app.models import Track
+from flask import render_template, request
+from app import app, db
+import pandas as pd
+from app.models import Artist
 
 @app.route('/')
 def index():
@@ -10,4 +9,28 @@ def index():
 
 @app.route('/artists_overview', methods=['GET'])
 def artists_overview():
-    return render_template('artists_overview.html')
+    artists = Artist.query.all()
+    search = request.args.get('search', '')
+    if search:
+        # Filter artists by name using the search term
+        artists = Artist.query.filter(Artist.artist_name.ilike(f'%{search}%')).all()
+    else:
+        # Show all artists if no search term is provided
+        artists = Artist.query.all()
+    return render_template('artists_overview.html', artists=artists)
+
+def load_data():
+    file_path = 'artists.csv'
+    data = pd.read_csv(file_path)
+    
+    for _, row in data.iterrows():
+        artist = Artist(
+            artist_name=row['Artist Name(s)'],
+            need_to_explore=row['Need to Explore'],
+            looked_at=row['Looked At'],
+            artist_playlist=row['Artist Playlist'],
+            one_of_each=row.get('One of Each', ''),
+            notes=row.get('Notes', '')
+        )
+        db.session.add(artist)
+    db.session.commit()
